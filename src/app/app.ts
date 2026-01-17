f1babaaefdad8f545680bad71cb069d81d174b8b
Allwin-Franklin <allwinanto360@gmail.com>
2026-01-18 00:26:26 +0530

    Create basic application structure with header component & Home page

---
+++ src/app/app.ts
@@ -0,0 +1,26 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './component/header/header';
+import { Home } from './home/home';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Header, Home],
+  template: `
+    <app-header />
+    <main>
+      <app-home />
+    </main>
+    <router-outlet />
+  `,
+  styles: [
+    `
+      main {
+        padding: 16px;
+      }
+    `
+  ],
+})
+export class App {
+  protected readonly title = signal('first-ng-app');
+}
